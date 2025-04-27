9495d404decdfe110b4fbb5debd9db22e5245218
Rick Busdiecker <rick@busdiecker.org>
2025-04-26 20:50:14 -0700

    Overhauled to use file block size, plus general cleanup

--- eliminate_terminal_nulls.c
+++ eliminate_terminal_nulls.c
@@ -6,8 +6,10 @@
 /* Table of Contents */
 /* Headers, etc */
 /* Function prototypes */
-/* main(int,char*) */
 /* eliminate_terminal_nulls(char*) */
+/* fstat_or_fail (int,struct stat*) */
+/* main(int,char*) */
+/* open_or_fail(char*,int) */
 /* scan_block(int,char*,size_t) */
 
 /* Headers, etc */
@@ -22,13 +24,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 /* Function prototypes */
+
 static
 int
 eliminate_terminal_nulls (char* pathname);
+
+static
+int
+fstat_or_fail (int          descriptor,
+               struct stat* status);
 
 int
 main (int    argument_count,
@@ -36,76 +44,15 @@ main (int    argument_count,
 
 static
 int
+open_or_fail (char* pathname,
+              int flags);
+
+static
+int
 scan_block (int    descriptor,
             int    offset,
             char*  block,
             size_t size);
-
-/* main(int,char*) */
-
-int
-main (int    argument_count,
-      char** argument_vector)
-{
-    int getopt_result;
-
-    while (1)
-    {
-        static struct option long_options [] =
-            {
-                {0, 0, 0, 0}
-            };
-
-        int option_index = 0;
-        getopt_result = getopt_long (argument_count, argument_vector, "abc:d:f:",
-                         long_options, &option_index);
-
-        /* Detect the end of the options. */
-        if (getopt_result == -1)
-        {
-            break;
-        }
-
-        switch (getopt_result)
-        {
-         case 0:
-            /* If this option set a flag, do nothing else now. */
-            if (long_options [option_index].flag != 0)
-            {
-                break;
-            }
-            printf ("option %s", long_options [option_index].name);
-            if (optarg)
-            {
-                printf (" with arg %s", optarg);
-            }
-            printf ("\n");
-            break;
-
-         case '?':
-            /* getopt_long already printed an error message. */
-            exit (1);
-            break;
-
-         default:
-            fprintf (stderr, "Unrecognized option: -%c\n", getopt_result);
-            exit (1);
-        }
-    }
-
-    /* Process any remaining command line arguments (not options). */
-    for (; optind < argument_count; ++optind)
-    {
-        char* argument = argument_vector [optind];
-
-        if (eliminate_terminal_nulls (argument) == 1)
-        {
-            printf ("%s\n", argument);
-        }
-    }
-
-    return 0;
-}
 
 /* eliminate_terminal_nulls(char*) */
 
@@ -123,22 +70,8 @@ eliminate_terminal_nulls (char* pathname)
     int           result                    = (int)0;
     struct stat   status_buffer;
 
-    descriptor = open (pathname, O_RDWR | O_LARGEFILE);
-    if (descriptor < 0)
-    {
-        perror ("open(2): ");
-        fprintf (stderr, "%s:%d: In eliminate_terminal_nulls, open(2) failed.\n",
-                 __FILE__, __LINE__);
-        exit (1);
-    }
-
-    if (fstat (descriptor, &status_buffer) != 0)
-    {
-        perror ("fstat(2): ");
-        fprintf (stderr, "%s:%d: In eliminate_terminal_nulls, fstat(2) failed.\n",
-                 __FILE__, __LINE__);
-        exit (1);
-    }
+    descriptor = open_or_fail (pathname, O_RDWR | O_LARGEFILE);
+    (void)fstat_or_fail (descriptor, &status_buffer);
 
     file_bytes                = status_buffer.st_size;
     file_block_size           = status_buffer.st_blksize;
@@ -208,6 +141,110 @@ eliminate_terminal_nulls (char* pathname)
         exit (1);
     }
 
+    return result;
+}
+
+/* fstat_or_fail (int,struct stat*) */
+
+static
+int
+fstat_or_fail (int          descriptor,
+               struct stat* status)
+{
+    int result = fstat (descriptor, status);
+
+    if (result != 0)
+    {
+        perror ("fstat(2): ");
+        fprintf (stderr, "%s:%d: In fstat_or_fail, fstat(2) failed.\n",
+                 __FILE__, __LINE__);
+        exit (1);
+    }
+    return result;
+}
+
+/* main(int,char*) */
+
+int
+main (int    argument_count,
+      char** argument_vector)
+{
+    int getopt_result;
+
+    while (1)
+    {
+        static struct option long_options [] =
+            {
+                {0, 0, 0, 0}
+            };
+
+        int option_index = 0;
+        getopt_result = getopt_long (argument_count, argument_vector, "",
+                         long_options, &option_index);
+
+        /* Detect the end of the options. */
+        if (getopt_result == -1)
+        {
+            break;
+        }
+
+        switch (getopt_result)
+        {
+         case 0:
+            /* If this option set a flag, do nothing else now. */
+            if (long_options [option_index].flag != 0)
+            {
+                break;
+            }
+            printf ("option %s", long_options [option_index].name);
+            if (optarg)
+            {
+                printf (" with arg %s", optarg);
+            }
+            printf ("\n");
+            break;
+
+         case '?':
+            /* getopt_long already printed an error message. */
+            exit (1);
+            break;
+
+         default:
+            fprintf (stderr, "Unrecognized option: -%c\n", getopt_result);
+            exit (1);
+        }
+    }
+
+    /* Process any remaining command line arguments (not options). */
+    for (; optind < argument_count; ++optind)
+    {
+        char* argument = argument_vector [optind];
+
+        if (eliminate_terminal_nulls (argument) == 1)
+        {
+            printf ("%s\n", argument);
+        }
+    }
+
+    return 0;
+}
+
+/* open_or_fail(char*,int) */
+
+static
+int
+open_or_fail (char* pathname,
+              int   flags)
+{
+    int result = open (pathname, flags);
+
+    if (result < 0)
+    {
+        perror ("open(2): ");
+        fprintf (stderr, "%s:%d: In open_or_fail, open(2) failed.\n",
+                 __FILE__, __LINE__);
+        exit (1);
+    }
     return result;
 }
 
